a1edc619748b713d06e55ea6d1c4897dd005a730
Mert <bilgic.mert44@hotmail.com>
2020-12-18 01:19:24 +0300

    Risk status was checked without using data

--- app/helper/calc_risk.py
+++ app/helper/calc_risk.py
@@ -1,19 +1,66 @@
-def calc_additional_risks(age, polluted, hypertension, diabetes, cardiovascular, respiratory, immune):
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
 
-    risk = 0
+def calc_risk_percentage(risk_score=10,density = 0):
+    """
+    Antecedent:
+        Bulanık bir kontrol sistemi için önceki (giriş / sensör) değişken.
+
+    Consequent:
+        Bulanık bir kontrol sistemi için sonuç (çıktı / kontrol) değişkeni.
+    """
+    ##Risk oranı
+    risk = ctrl.Antecedent(np.arange(0, 10, 0.5), 'risk')
+
+    #Dışarda harcanan zaman
+    time_spent_outside = ctrl.Consequent(np.arange(1, 15, 0.1), 'outside')
+
+
+    # Kümenin öğeleri üyelik derecelerine göre eşlenir
+    risk['high'] = fuzz.zmf(risk.universe, 0, 10)
+    risk['low'] = fuzz.smf(risk.universe, 0, 10)
+
+    # Kümenin öğeleri üyelik derecelerine göre eşlenir
+    time_spent_outside['long'] = fuzz.zmf(time_spent_outside.universe, 1, 15)
+    time_spent_outside['short'] = fuzz.smf(time_spent_outside.universe, 1,15)
+
+    #Bulanık bir kontrol sisteminde Antecedent'ı Consequent'a bağlayan kural.
+    rule1 = ctrl.Rule(risk['high'], time_spent_outside['long'])
+    rule2 = ctrl.Rule(risk['low'], time_spent_outside['short'])
+
+    # controller
+    riskController = ctrl.ControlSystem([rule1, rule2])
+
+    # simulation
+    riskSimulator = ctrl.ControlSystemSimulation(riskController)
+
+    # load the value in the simulator
+    riskSimulator.input['risk'] = risk_score
+
+    # process
+    riskSimulator.compute()
+
+    # output
+    result = riskSimulator.output['outside'] * density
+
+    return result
+
+
+def calc_additional_risks_score(age, polluted, hypertension, diabetes, cardiovascular, respiratory, immune):
+
+    risk = 1
 
     if age <= 10: risk += 0.5
     elif age >= 80: risk += 1.5
     elif age >= 60: risk += 1
-    elif age >= 50: risk += 0.5
+    elif age >= 50: risk += 0.8
 
     if polluted: risk += 1
-    if hypertension: risk += 1.5
+    if hypertension: risk += 1.4
     if diabetes: risk += 1.5
-    if cardiovascular: risk += 1
-    if respiratory: risk += 3
-    if immune: risk += 4
-
-    if risk > 10: risk = 10
+    if cardiovascular: risk += 1.7
+    if respiratory: risk += 2.5
+    if immune: risk += 2.5
 
     return risk
--- app/src/views.py
+++ app/src/views.py
@@ -1,6 +1,7 @@
+from datetime import time
 import streamlit as st
 
-from app.helper.calc_risk import calc_additional_risks
+from app.helper.calc_risk import calc_additional_risks_score, calc_risk_percentage
 
 def main ():
     st.title('Covid-19 Tarama')
@@ -11,7 +12,15 @@ def main ():
 
     breath_diff = st.slider('Nefes alma zorlugu:', 0.0, 10.0, 0.0)
 
+    calculate_covid = st.button('Hesapla')
+
+    st.title('Risk Kontrolü')
+
+    st.info("Bu kontrol sırasında herhangi bir veri kullanılmadı kontrol test amaçlı yapıldı.")
+
     age = st.slider('Yasiniz:', 1, 100, 25)
+
+    density = st.slider('Bölgedeki covid-19 durumu :', 0.0, 10.0, 5.0)
 
     polluted = st.checkbox('Hava kirliligi olan bir yerde mi yasiyorsun?')
 
@@ -25,13 +34,12 @@ def main ():
 
     immune = st.checkbox('Bagisiklik sistemin zayif mi?')
 
-    calculate = st.button('Hesapla')
+    calculate_risk = st.button('Hesapla ')
 
-
-    if(calculate is True):
-        result = calc_additional_risks(age,polluted,hypertension,diabetes,cardiovascular,respiratory,immune)
-        st.subheader(f"Risk Score: {result}")
-    else:
-        pass
+    if(calculate_risk is True):
+        additional_risks = (age,polluted,hypertension,diabetes,cardiovascular,respiratory,immune)
+        result = calc_additional_risks_score(*additional_risks)
+        result = calc_risk_percentage(result,density)
+        st.subheader(f"Covid-19'a yakalanma ihtimaliniz %{result}")
 
 
