bf72d8d51e623c2a0c6ed36d7a721381731e7840
Mert <bilgic.mert44@hotmail.com>
2020-12-20 00:32:40 +0300

    covid-19 olasılığı hesaplandı

--- app/helper/calc_risk.py
+++ app/helper/calc_risk.py
@@ -1,66 +1,26 @@
+
 import numpy as np
-import skfuzzy as fuzz
-from skfuzzy import control as ctrl
 
-def calc_risk_percentage(risk_score=10,density = 0):
-    """
-    Antecedent:
-        Bulanık bir kontrol sistemi için önceki (giriş / sensör) değişken.
+from app.helper.mf import MF
 
-    Consequent:
-        Bulanık bir kontrol sistemi için sonuç (çıktı / kontrol) değişkeni.
-    """
-    ##Risk oranı
-    risk = ctrl.Antecedent(np.arange(0, 10, 0.5), 'risk')
+from pyit2fls import IT2FS_Gaussian_UncertMean
+class Risk():
 
-    #Dışarda harcanan zaman
-    time_spent_outside = ctrl.Consequent(np.arange(1, 15, 0.1), 'outside')
+    @staticmethod
+    def calc_additional_risks_score(age, polluted, hypertension, diabetes, cardiovascular, respiratory, immune):
 
+        risk = 0
 
-    # Kümenin öğeleri üyelik derecelerine göre eşlenir
-    risk['high'] = fuzz.zmf(risk.universe, 0, 10)
-    risk['low'] = fuzz.smf(risk.universe, 0, 10)
+        if age <= 10: risk += 0.55
+        elif age >= 80: risk += 1.55
+        elif age >= 60: risk += 1.00
+        elif age >= 50: risk += 0.50
 
-    # Kümenin öğeleri üyelik derecelerine göre eşlenir
-    time_spent_outside['long'] = fuzz.zmf(time_spent_outside.universe, 1, 15)
-    time_spent_outside['short'] = fuzz.smf(time_spent_outside.universe, 1,15)
+        if polluted: risk += 1.0
+        if hypertension: risk += 1.55
+        if diabetes: risk += 1.55
+        if cardiovascular: risk += 1.5
+        if respiratory: risk += 3.05
+        if immune: risk += 4.05
 
-    #Bulanık bir kontrol sisteminde Antecedent'ı Consequent'a bağlayan kural.
-    rule1 = ctrl.Rule(risk['high'], time_spent_outside['long'])
-    rule2 = ctrl.Rule(risk['low'], time_spent_outside['short'])
-
-    # controller
-    riskController = ctrl.ControlSystem([rule1, rule2])
-
-    # simulation
-    riskSimulator = ctrl.ControlSystemSimulation(riskController)
-
-    # load the value in the simulator
-    riskSimulator.input['risk'] = risk_score
-
-    # process
-    riskSimulator.compute()
-
-    # output
-    result = riskSimulator.output['outside'] * density
-
-    return result
-
-
-def calc_additional_risks_score(age, polluted, hypertension, diabetes, cardiovascular, respiratory, immune):
-
-    risk = 1
-
-    if age <= 10: risk += 0.5
-    elif age >= 80: risk += 1.5
-    elif age >= 60: risk += 1
-    elif age >= 50: risk += 0.8
-
-    if polluted: risk += 1
-    if hypertension: risk += 1.4
-    if diabetes: risk += 1.5
-    if cardiovascular: risk += 1.7
-    if respiratory: risk += 2.5
-    if immune: risk += 2.5
-
-    return risk
+        return risk
